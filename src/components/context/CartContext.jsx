fcbf5e6f0b703dba8f6aaace26ede4ecbcb4a270
itswakana <100365678+ItsWakana@users.noreply.github.com>
2023-05-18 23:15:27 +0100

    make handleCartAdd method more performant

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -12,26 +12,39 @@ export const CartProvider = ({ children }) => {
         return acc + curr.qty;
     }, 0);
 
+    // const handleCartAdd = (product, desiredQty) => {
+    //     // console.log(`Product: ${product.name}`);
+    //     // console.log(`Quantity: ${desiredQty}`);
+    //     if (desiredQty < 1) return;
+    //     setItemAdded(true);
+    //     let hasProductInCart = false;
+    //     const updatedCart = cart.map((item) => {
+    //         if (item.id === product.id) {
+    //                 hasProductInCart = true;
+    //                 return { ...item, qty: item.qty + desiredQty };
+    //             }
+    //             return item;
+    //         });
+
+    //         if (hasProductInCart) {
+    //             setCart(updatedCart);
+    //         } else {
+    //             setCart([...cart, {...product, qty: desiredQty}]);
+    //         }
+    //     }
+
     const handleCartAdd = (product, desiredQty) => {
-        // console.log(`Product: ${product.name}`);
-        // console.log(`Quantity: ${desiredQty}`);
         if (desiredQty < 1) return;
         setItemAdded(true);
-        let hasProductInCart = false;
-        const updatedCart = cart.map((item) => {
-            if (item.id === product.id) {
-                    hasProductInCart = true;
-                    return { ...item, qty: item.qty + desiredQty };
-                }
-                return item;
-            });
+        setCart((cart) => 
+            cart.every((item) => item.id !== product.id)
+                ? [...cart, {...product, qty: desiredQty}]
+                : cart.map((item) =>
+                        item.id === product.id ? {...item, qty: item.qty + desiredQty } : item,
+                        ),
+        );
+    };
 
-            if (hasProductInCart) {
-                setCart(updatedCart);
-            } else {
-                setCart([...cart, {...product, qty: desiredQty}]);
-            }
-        }
     return (
         <CartContext.Provider value={{
             cart, totalQty, handleCartAdd, itemAdded, setItemAdded
