00b8202893674eb2cda8ce2ff23f7e056c38922f
wakana <wakanawardrobe@gmail.com>
2023-06-08 16:43:35 +0100

    Integrate firebase database with shopping cart and add log in feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,13 @@ import NavigationContext from "./context/NavigationContext";
 import { Link } from "react-router-dom";
 import BasketIcon from "./BasketIcon";
 import { ThemeContext } from "./context/ThemeContext";
+import CartContext from "./context/CartContext";
 
 const Navbar = () => {
 
     const { showItems, toggleMobileMenu } = useContext(NavigationContext);
     const { darkTheme, toggleDarkTheme } = useContext(ThemeContext);
+    const { handleLoginClick, isLoggedIn, user } = useContext(CartContext);
 
     useEffect(() => {
 
@@ -30,6 +32,14 @@ const Navbar = () => {
           <div className="desktop-menu">
             <Link className="nav-link" to="/">HOME</Link>
             <Link className="nav-link" to="/store">GAMES</Link>
+            {/* <div onClick={handleLoginClick} className="nav-link">{`${isLoggedIn ? 'SIGN OUT' : 'SIGN IN'}`}</div> */}
+            {isLoggedIn ? 
+            <div className="nav-link logged">
+              <a onClick={handleLoginClick}>SIGN OUT</a>
+              {/* <p>{user.displayName}</p> */}
+              <img className="profile-icon" src={user.photoURL} alt="" />
+            </div> : 
+            <div onClick={handleLoginClick} className="nav-link">SIGN IN</div>}
           </div>
           <div className="nav-bar__right-side">
           <div>
--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -1,87 +1,184 @@
 import { useState, useMemo, useEffect, createContext } from "react";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    limit,
+    onSnapshot,
+    setDoc,
+    updateDoc,
+    doc,
+    serverTimestamp,
+    getDoc,
+    deleteField
+} from 'firebase/firestore';
+
+import {
+    getAuth,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+} from 'firebase/auth';
+
+import { db } from "../../main";
 
 const CartContext = createContext({});
 
 export const CartProvider = ({ children }) => {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
     const [cart, setCart] = useState([]);
-
     const [itemAdded, setItemAdded] = useState(false);
-
-    // const [maxItemQuantityErr, setMaxItemQuantityErr] = useState(false);
     const [cartError, setCartError] = useState('');
 
     useEffect(() => {
+
+        if (isLoggedIn) {
+            getFirebaseData();
+            return;
+        }
+
         const localStorageData = JSON.parse(localStorage.getItem('cart'));
 
         if (localStorageData) {
             setCart(localStorageData);
         }
 
-    },[]);
-    // const totalQty = cart.reduce((acc, curr) => {
-    //     return acc + curr.qty;
-    // }, 0);
+    },[isLoggedIn]);
 
     const totalQty = useMemo(() => cart.reduce((acc, curr) => {
         return acc + curr.qty;
     }, 0),[cart]);
 
-    // const totalPrice = cart.reduce((acc, curr) => acc + (curr.price * curr.qty), 0);
     const totalPrice = useMemo(() => cart.reduce((acc, curr) => acc + (curr.price * curr.qty), 0),[cart]);
 
-    const handleQuantityChange = (quantity, productId) => {
-        setItemAdded(true);
-        // const updatedCart = cart.map((item) => item.id === productId ? { ...item, qty: quantity} : item);
+    const getFirebaseData = async (userId) => {
 
-        // localStorage.setItem('cart', JSON.stringify(updatedCart));
+        const docRef = doc(db, `/${userId}`, "cart");
+        const docSnap = await getDoc(docRef);
 
-        // setCart(updatedCart);
+        if (docSnap.exists()) {
+            const arrayData = Object.values(docSnap.data());
+            return arrayData;
+        } else {
+            return null;
+        }
+    }
+    
+    const handleLoginClick = async () => {
 
-        setCart((cart) => {
-            const updatedCart = cart.map((item) => item.id === productId ? { ...item, qty: quantity} : item);
+        if (isLoggedIn) {
+            signOut(getAuth());
+            setUser(null);
+            setIsLoggedIn(false);
+            return;
+        }
 
-            localStorage.setItem('cart', JSON.stringify(updatedCart));
-
-            return updatedCart;
-        });
-        // setCart((cart) => cart.map((item) => item.id === productId ? {...item, qty: quantity} : item));
-
+        try {
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(getAuth(), provider);
+    
+            const auth = getAuth();
+            setUser(auth.currentUser);
+            setIsLoggedIn(true);
+    
+            const cartData = await getFirebaseData(auth.currentUser.uid);
+            if (cartData) {
+                setCart(cartData);
+            }
+   
+        } catch(err) {
+            console.log(err);
+        }
     }
 
-    const removeCartItem = (productId) => {
-        // setCart((cart) => cart.filter((item) => item.id !== productId));
+    const handleQuantityChange = async (quantity, productId) => {
+        setItemAdded(true);
 
-        setCart((cart) => {
-            const updatedCart = cart.filter((item) => item.id !== productId);
+        const updatedCart = cart.map((item) => item.id === productId ? {...item, qty: quantity} : item);
 
-            localStorage.setItem('cart', JSON.stringify(updatedCart));
+        
+        if (isLoggedIn) {
+            
+            const cartRef = doc(db, `/${user.uid}`, "cart");
+            
+            const updatedItem = updatedCart.find((item) => item.id === productId);
 
-            return updatedCart;
-        });
+            await updateDoc(cartRef, {
+                [productId]: updatedItem
+            });
+
+        } else {
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+        }
+
+        setCart(updatedCart);
     }
 
-    const handleCartAdd = (product) => {
+    const removeCartItem = async (productId) => {
+
+        const updatedCart = cart.filter((item) => item.id !== productId);
+
+
+        if (isLoggedIn) {
+            const cartRef = doc(db, `/${user.uid}`, "cart");
+
+            await updateDoc(cartRef, {
+                [productId]: deleteField()
+            });
+        } else {
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+        }
+
+        setCart(updatedCart);
+    }
+
+    const handleCartAdd = async (product) => {
         setItemAdded(true);
-        setCart((cart) => {
-            const updatedCart = cart.every((item) => item.id !== product.id) ? [...cart, {...product, qty: 1}]
-                : cart.map((item) => {
-                    if (item.id === product.id && item.qty === 5) {
-                        setCartError("Max quantity reached!");
-                        return item;
-                    }
-                    if (item.id === product.id) {
-                        return {...item, qty: item.qty + 1}
-                    } else {
-                        return item;
+        const updatedCart = cart.every((item) => item.id !== product.id) ? [...cart, {...product, qty: 1}]
+            : cart.map((item) => {
+                if (item.id === product.id && item.qty === 5) {
+                    setCartError("Max quantity reached!");
+                    return item;
+                }
+                if (item.id === product.id) {
+                    return {...item, qty: item.qty + 1}
+                } else {
+                    return item;
+                }
+        });
+            
+        if (isLoggedIn) {
+            setCart(updatedCart);
+
+            const cartDocRef = doc(db, `/${user.uid}`, "cart");
+            const cartSnapshot = await getDoc(cartDocRef);
+
+            if (!cartSnapshot.exists()) {
+                await setDoc(doc(db, user.uid, "cart") , {
+                    [product.id]: {
+                        ...product,
+                        qty: 1
                     }
                 });
-            
-            localStorage.setItem('cart', JSON.stringify(updatedCart));
+                return;
+            }
 
-            return updatedCart;
-        });
-    };
+            const updatedProduct = updatedCart.find((item) => item.id === product.id);
+
+            await updateDoc(cartDocRef, {
+                [product.id]: updatedProduct
+            });
+
+        } else {
+            localStorage.setItem("cart", JSON.stringify(updatedCart));
+            setCart(updatedCart);
+        }
+        };
 
     const resetError = () => {
         setCartError('');
@@ -90,7 +187,7 @@ export const CartProvider = ({ children }) => {
 
     return (
         <CartContext.Provider value={{
-            cart, totalQty, totalPrice, handleCartAdd, itemAdded, setItemAdded, handleQuantityChange, removeCartItem, cartError, setCartError, resetError
+            cart, totalQty, totalPrice, handleCartAdd, itemAdded, setItemAdded, handleQuantityChange, removeCartItem, cartError, setCartError, resetError, handleLoginClick, isLoggedIn, user
         }}>
             {children}
         </CartContext.Provider>
