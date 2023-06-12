8a351c43d3a60e01d6d0c61b2aec814c7d825423
wakana <wakanawardrobe@gmail.com>
2023-06-12 13:00:33 +0100

    implement firebase storage for user accounts shopping cart

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -11,6 +11,7 @@ import {
     updateDoc,
     doc,
     serverTimestamp,
+    getDoc
 } from 'firebase/firestore';
 
 import {
@@ -39,13 +40,23 @@ export const CartProvider = ({ children }) => {
     const [cartError, setCartError] = useState('');
 
     useEffect(() => {
+
+
+        //when the application mounts, we first want to check our users localStorage and set that if it exists already.
+
+        if (isLoggedIn) {
+            getFirebaseData();
+            return;
+        }
+        //we can put the isLoggedIn bool as a useEffect dependency, then at the top here we could check if isLoggedIn is true. If it is we can check the users storage on firebase and if there is any data on the cart on there. If there is we can get the data from there, and simply return out of the useEffect.
+
         const localStorageData = JSON.parse(localStorage.getItem('cart'));
 
         if (localStorageData) {
             setCart(localStorageData);
         }
 
-    },[]);
+    },[isLoggedIn]);
 
     const totalQty = useMemo(() => cart.reduce((acc, curr) => {
         return acc + curr.qty;
@@ -53,6 +64,16 @@ export const CartProvider = ({ children }) => {
 
     const totalPrice = useMemo(() => cart.reduce((acc, curr) => acc + (curr.price * curr.qty), 0),[cart]);
 
+    const getFirebaseData = async (userId) => {
+        const docRef = doc(db, userId, "cart");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            return docSnap.data().data;
+        } else {
+            console.log('No such data!');
+        }
+    }
     const handleLoginClick = async () => {
 
         if (isLoggedIn) {
@@ -62,17 +83,22 @@ export const CartProvider = ({ children }) => {
             return;
         }
 
-        const provider = new GoogleAuthProvider();
-        await signInWithPopup(getAuth(), provider);
-
-        const auth = getAuth();
-        // const user = auth.currentUser;
-        setUser(auth.currentUser);
-        setIsLoggedIn(true);
-
-        await setDoc(doc(db, auth.currentUser.uid, "cart"), {
-            data: cart
-        });
+        try {
+            const provider = new GoogleAuthProvider();
+            await signInWithPopup(getAuth(), provider);
+    
+            const auth = getAuth();
+            setUser(auth.currentUser);
+            setIsLoggedIn(true);
+    
+            const cartData = await getFirebaseData(auth.currentUser.uid);
+            setCart(cartData);
+            // await setDoc(doc(db, auth.currentUser.uid, "cart"), {
+            //     data: cart
+            // });
+        } catch(err) {
+            console.log(err);
+        }
     }
 
     const handleQuantityChange = (quantity, productId) => {
@@ -114,15 +140,23 @@ export const CartProvider = ({ children }) => {
                 }
             });
             
-            localStorage.setItem('cart', JSON.stringify(updatedCart));
-            setCart(updatedCart);
-            // return updatedCart;
-            console.log(user);
-            if (user) {
+
+            if (isLoggedIn) {
                 await setDoc(doc(db, user.uid, "cart"), {
-                    data: cart
+                    data: updatedCart
                 });
+            } else {
+                localStorage.setItem("cart", JSON.stringify(updatedCart));
             }
+            setCart(updatedCart);
+            // localStorage.setItem('cart', JSON.stringify(updatedCart));
+            // setCart(updatedCart);
+
+            // if (user) {
+            //     await setDoc(doc(db, user.uid, "cart"), {
+            //         data: updatedCart
+            //     });
+            // }
         };
 
     const resetError = () => {
